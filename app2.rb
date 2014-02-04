8bc23244b8c9d7cdfbb6c6b9f491f3a3835011dd
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-02-04 16:55:31 +0000

    separated layout and idnex for app2

--- app2.rb
+++ app2.rb
@@ -7,40 +7,50 @@ get '/' do
 end
 
 get '/chuchu' do
-  "hello world!"
+  if request.xhr? # is an ajax request
+    "hello world!"
+  else 
+    erb :tutu
+  end
 end
 
 __END__
 
+@@layout
+  <!DOCTYPE html>
+  <html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Demo</title>
+    </head>
+    <body>
+        <a href="http://jquery.com/">jQuery</a>
+        <div class="result"></div>
+        <script src="jquery.js"></script>
+        <%= yield %>
+    </body>
+  </html>
+
+@@tutu
+  <h1>Not an Ajax Request!</h1>
+
 @@index
-<!DOCTYPE html>
-<html>
-  <head>
-      <meta charset="utf-8" />
-      <title>Demo</title>
-  </head>
-  <body>
-      <a href="http://jquery.com/">jQuery</a>
-      <div class="result"></div>
-      <script src="jquery.js"></script>
-      <script>
-        var param = "chuchu param";
-        var handler = function( data, textStatus, jqXHR, param ) {
-          $( ".result" ).html( data );
-          alert( "Load was performed.\n"+
-                 "$data = "+data+
-                 "\ntextStatus = "+textStatus+
-                 "\njqXHR = "+JSON.stringify(jqXHR)+
-                 "\nparam = "+param );
-        };
-        $( document ).ready(function() {
-            $( "a" ).click(function( event ) {
-                event.preventDefault();
-                $.get( "/chuchu", function(data, textStatus, jqXHR ) {
-                  handler( data, textStatus, jqXHR, param);
-                });
+  <script>
+    var param = "chuchu param";
+    var handler = function( data, textStatus, jqXHR, param ) {
+      $( ".result" ).html( data );
+      alert( "Load was performed.\n"+
+             "$data = "+data+
+             "\ntextStatus = "+textStatus+
+             "\njqXHR = "+JSON.stringify(jqXHR)+
+             "\nparam = "+param );
+    };
+    $( document ).ready(function() {
+        $( "a" ).click(function( event ) {
+            event.preventDefault();
+            $.get( "/chuchu", function(data, textStatus, jqXHR ) {
+              handler( data, textStatus, jqXHR, param);
             });
         });
-      </script>
-  </body>
-</html>
+    });
+  </script>
