033470f00c89fc0ae25f8b6a9ba0f2b0c4bc20f5
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-03-08 16:15:31 +0000

    app.set('port', (process.env.PORT || 3000)); and app.get('port')

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var app = express();
 var path = require('path');
 var expressLayouts = require('express-ejs-layouts');
 
+app.set('port', (process.env.PORT || 3000));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 //app.set('layout', 'myLayout') // defaults to 'layout'  '
 
 app.use(expressLayouts);
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Express' });
@@ -27,7 +30,7 @@ app.get('/chuchu', function (req, res) {
   }
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get('port'), function () {
 
   var host = server.address().address
   var port = server.address().port
